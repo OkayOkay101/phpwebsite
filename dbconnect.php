379da51e58495447df8c456798adcbdc0a248042
OkayOkay101 <78838036+OkayOkay101@users.noreply.github.com>
2025-01-11 00:11:57 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,14 @@
 <!--<link href="css/style.css" rel="stylesheet">-->
 <?php
-$con = mysqli_connect("localhost", "root", "", "mydata") or die("เกิดข้อผิดพลาดเกิดขึ้น");
+// Database connection
+$con = mysqli_connect("localhost", "root", "", "mydata");
+
+// Check connection
+if (!$con) {
+    die("เกิดข้อผิดพลาด: " . mysqli_connect_error());
+}
+
+// Set character encoding to UTF-8
 mysqli_set_charset($con, "utf8");
+?>
+
